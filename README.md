a1a6eda05a3a6d38549f46d41353d967c189e9b9
Shubham <shubhamshukla19981703@gmail.com>
2024-04-09 01:35:37 +0530

    @shubham changed code to filter top rated restaurant

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ we use curly braces to inport named exports.
 
 The key difference between named exports and default exports lies in their usage. Named exports are useful when a module exports multiple things, like objects or functions. On the other hand, default exports are used when a module exports a single thing, like a function or a class.
 
+ # React Hooks
+
+ (Normal JS utility functions)
+Hooks is a javascript function.
+
+- useState() -  
+- useEffect()
+
+whenever the a state variable updates react  re renders the component.
--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,11 @@ import resList from "../utils/mockData";
 const Body = () => {
     return (
         <div className="body">
-            <div className='search'>Search</div>
+            <div className='filter'>
+                <button className='filter-btn' onClick={() => { console.log("Button Clicked") }}>Top Rated Restaurant</button>
+            </div>
             <div className='restaurant-container'>
-                {resList.map(resData => <RestaurantCard key={resData.info.id}resCard={resData} />)}
+                {resList.map(resData => <RestaurantCard key={resData.info.id} resCard={resData} />)}
             </div>
         </div>
     )
